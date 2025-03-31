c92a5e92f3a342c5d74975c81b92bcefe911f5b7
fkgrkyr <fkgrkyr@gmail.com>
2025-03-31 22:30:14 +0900

    FEAT : 버튼추가

---
+++ 멋사 과제/1주차 과제 에어비엔비 클론코딩/main.js
@@ -0,0 +1,30 @@
+// 플러스 버튼 ID로 선택
+const plusButton = document.getElementById("plusButton");
+// 마이너스 버튼 ID로 선택
+const minusButton = document.getElementById("minusButton");
+// 광진구 집 숙박비 ID로 선택
+const card1Value = document.getElementById("card1Value");
+
+// HTML에 저장된 광진구 집 숙박비 값 문자열 -> 정수로 변경
+let value = Number(card1Value.textContent);
+
+// HTML에 저장된 광진구 집 숙박비 값 toLocaleString 처리
+function makeLocaleString(){
+    card1Value.textContent = value.toLocaleString();
+}
+
+makeLocaleString();
+
+// 플러스 버튼 눌렀을 때 광진구 집 숙박비 값 +10,000원
+plusButton.addEventListener("click", ()=>{
+    value+=10000;
+    card1Value.textContent = value;
+    makeLocaleString();
+})
+
+// 마이너스 버튼 눌렀을 때 광진구 집 숙박비 값 -10,000원
+minusButton.addEventListener("click", ()=>{
+    value -=10000;
+    card1Value.textContent = value;
+    makeLocaleString();s
+})
